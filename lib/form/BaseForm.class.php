34f536a8840cfaffdb358425a18ae0983a5d18f6
Xavier Lacot <xavier@lacot.org>
2011-09-17 00:33:40 +0200

    initial commit - generated the symfony project

---
+++ lib/form/BaseForm.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Base project form.
+ * 
+ * @package    sfDoctrineRestGeneratorPlugin-demo
+ * @subpackage form
+ * @author     Xavier Lacot <xavier@lacot.org> 
+ * @version    SVN: $Id: BaseForm.class.php 20147 2009-07-13 11:46:57Z FabianLange $
+ */
+class BaseForm extends sfFormSymfony
+{
+}
