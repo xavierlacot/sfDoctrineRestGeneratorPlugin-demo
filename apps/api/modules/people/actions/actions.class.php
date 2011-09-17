10d980ada51e0e5798e8d3eca08b56063bf0fd93
Xavier Lacot <xavier@lacot.org>
2011-09-17 12:07:52 +0200

    created both backend and api applications, published the plugins assets

---
+++ apps/api/modules/people/actions/actions.class.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * people actions.
+ *
+ * @package    sfDoctrineRestGeneratorPlugin-demo
+ * @subpackage people
+ * @author     Xavier Lacot <xavier@lacot.org>
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z xavier $
+ * @see        class::autopeopleActions
+ */
+class peopleActions extends autopeopleActions
+{
+}
