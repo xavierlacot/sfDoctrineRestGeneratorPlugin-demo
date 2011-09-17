dd953199f9337576c00c4d45c87fa587890b1f6d
Xavier Lacot <xavier@lacot.org>
2011-09-17 12:06:53 +0200

    added model files

---
+++ lib/filter/doctrine/PeopleFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * People filter form.
+ *
+ * @package    sfDoctrineRestGeneratorPlugin-demo
+ * @subpackage filter
+ * @author     Xavier Lacot <xavier@lacot.org>
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class PeopleFormFilter extends BasePeopleFormFilter
+{
+  public function configure()
+  {
+  }
+}
---
+++ lib/form/doctrine/PeopleForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * People form.
+ *
+ * @package    sfDoctrineRestGeneratorPlugin-demo
+ * @subpackage form
+ * @author     Xavier Lacot <xavier@lacot.org>
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class PeopleForm extends BasePeopleForm
+{
+  public function configure()
+  {
+  }
+}
